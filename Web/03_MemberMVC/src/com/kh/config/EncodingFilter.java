4e1195be00fe01e2d7984830efd12e2da4bcd6c0
tlseorb123 <tlseorb123@gmail.com>
2024-02-07 19:06:03 +0900

    Web

--- Web/03_MemberMVC/src/com/kh/config/EncodingFilter.java
+++ Web/03_MemberMVC/src/com/kh/config/EncodingFilter.java
@@ -1,6 +1,7 @@
-package com.kh;
+package com.kh.config;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,23 +9,20 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpFilter;
 
 
+@WebFilter(value = {"/*"}, initParams=@WebInitParam(name="encoding", value="utf-8"))
 public class EncodingFilter extends HttpFilter implements Filter {
        
- 
-
+   
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 한글처리 인코딩
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charst=utf-8");
 		
-		
-		// 사전작업의 내용이 서버상의 다음 컴포넌트(Servlet)에게 계속 적용되기 위한 작업
 		chain.doFilter(request, response);
 	}
-
-
-
 }
+
+
